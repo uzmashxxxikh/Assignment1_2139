bff76177bba8a60475be542512be620f1f616cec
uzmashxxxikh <uzmashxikh@hotmail.com>
2025-02-06 17:10:59 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Assignment1_COMP2139.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
